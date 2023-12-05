5cea647433205b8c4083766c85b49fa3a24c6689
Dilanka Liyanagama <prasadliyanagamad@gmail.com>
2023-12-05 12:50:13 +0530

    Add a Pie Chart according to the data

--- lib/pages/expences.dart
+++ lib/pages/expences.dart
@@ -2,6 +2,7 @@ import 'package:expence_master/models/expence.dart';
 import 'package:expence_master/widgets/add_new_expence.dart';
 import 'package:expence_master/widgets/expence_list.dart';
 import 'package:flutter/material.dart';
+import 'package:pie_chart/pie_chart.dart';
 
 class Expences extends StatefulWidget {
   const Expences({super.key});
@@ -30,10 +31,19 @@ class _ExpencesState extends State<Expences> {
         category: Category.work),
   ];
 
+  // PIE Chart
+  Map<String, double> dataMap = {
+    "Food": 0,
+    "Travel": 0,
+    "Leasure": 0,
+    "Work": 0,
+  };
+
   // add new expence
   void onAddNewExpence(ExpenceModel expence) {
     setState(() {
       _expenceList.add(expence);
+      calCategoryValues();
     });
   }
 
@@ -45,6 +55,7 @@ class _ExpencesState extends State<Expences> {
     final int removedIndex = _expenceList.indexOf(expence);
     setState(() {
       _expenceList.remove(expence);
+      calCategoryValues();
     });
     // show snackbar
     ScaffoldMessenger.of(context).showSnackBar(
@@ -55,6 +66,7 @@ class _ExpencesState extends State<Expences> {
             onPressed: () {
               setState(() {
                 _expenceList.insert(removedIndex, deletedExpence);
+                calCategoryValues();
               });
             }),
       ),
@@ -71,6 +83,53 @@ class _ExpencesState extends State<Expences> {
         );
       },
     );
+  }
+
+  double foodVal = 0;
+  double leasureVal = 0;
+  double travelVal = 0;
+  double workVal = 0;
+
+  void calCategoryValues() {
+    double foodValTotal = 0;
+    double leasureValTotal = 0;
+    double travelValTotal = 0;
+    double workValTotal = 0;
+
+    for (final expence in _expenceList) {
+      if (expence.category == Category.food) {
+        foodValTotal += expence.amount;
+      }
+      if (expence.category == Category.travel) {
+        travelValTotal += expence.amount;
+      }
+      if (expence.category == Category.leasure) {
+        leasureValTotal += expence.amount;
+      }
+      if (expence.category == Category.work) {
+        workValTotal += expence.amount;
+      }
+    }
+    setState(() {
+      foodVal = foodValTotal;
+      leasureVal = leasureValTotal;
+      travelVal = travelValTotal;
+      workVal = workValTotal;
+    });
+
+    // update the datamap
+    dataMap = {
+      "Food": foodVal,
+      "Travel": travelVal,
+      "Leasure": leasureVal,
+      "Work": workVal,
+    };
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    calCategoryValues();
   }
 
   @override
@@ -94,6 +153,7 @@ class _ExpencesState extends State<Expences> {
       ),
       body: Column(
         children: [
+          PieChart(dataMap: dataMap),
           ExpenceList(
             expenceList: _expenceList,
             onDeleteExpence: onDeleteExpence,
